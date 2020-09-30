d6b4dcb1b972c34e490a8aa477ac9e4256d73c15
Blen Wondimagegn <mekuriablen@gmail.com>
2020-09-30 22:55:18 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,26 +4,19 @@ class App < Sinatra::Base
   get '/newteam' do 
   erb :newteam
   end 
+  
+  get '/todos/:id' do
+        id = params[:id]
+        @todo = Todo.find_by_id(id)
+        erb :"todos/show"
+   end
 
    post '/newteam' do 
     erb:team
    end 
+   
 end
 
-  get '/team' do 
-        @team = Team.all
-        erb :team 
-    end
-
-    get '/t/new' do
-        erb :"todos/new"
-    end
-
-    get '/todos/:id' do
-        id = params[:id]
-        @todo = Todo.find_by_id(id)
-        erb :"todos/show"
-    end
 
     post '/todos' do
         # todo = Todo.new(name: params[:name], description: params[:description])
