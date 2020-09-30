53c5533afad80054f50cb6b60d4e84b1508466c4
Blen Wondimagegn <mekuriablen@gmail.com>
2020-09-30 22:52:37 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -9,3 +9,32 @@ class App < Sinatra::Base
     erb:team
    end 
 end
+
+  get '/team' do 
+        @team = Team.all
+        erb :"s/index"
+    end
+
+    get '/todos/new' do
+        erb :"todos/new"
+    end
+
+    get '/todos/:id' do
+        id = params[:id]
+        @todo = Todo.find_by_id(id)
+        erb :"todos/show"
+    end
+
+    post '/todos' do
+        # todo = Todo.new(name: params[:name], description: params[:description])
+        todo = Todo.new(params)
+        if todo.save
+            redirect "todos/#{todo.id}"
+        else
+            redirect "todos/new"
+        end
+    end
+
+
+
+end
